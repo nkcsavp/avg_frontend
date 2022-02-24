afacd32403a7e83eec4badeddf99b05734b118f9
Nicer_Wang <nicer_wang@qq.com>
2022-02-25 01:11:17 +0800

    feat:CI&CD

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from "./router"
-import ElementPlus from 'element-plus'
 import axios from "axios"
 import 'element-plus/dist/index.css'
 import store from './store'
@@ -12,4 +11,4 @@ axios.defaults.baseURL = "/api";
 //本地调试时使用以下代码[注意，提交时不要泄露服务器IP]
 // axios.defaults.baseURL = "http://localhost:8080/";
 
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
