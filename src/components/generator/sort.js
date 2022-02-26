ffbcdd2b2ab849c402d674b4f0fc57ebc5fcaffa
mingjie li <1913090@mail.nankai.edu.cn>
2022-02-26 10:18:07 +0800

    feat:sort-select

--- src/components/generator/sort.js
+++ src/components/generator/sort.js
@@ -3,51 +3,86 @@
 sort.js
  */
 export function getSortTypes() {
-    return{
+    return {
         /*
             在下方添加新增的算法名称
             Add new algorithms below
          */
         "bubble": "冒泡排序",
+        "select": "选择排序",
     }
 }
 
 export function getFunctions() {
-    return{
+    return {
         /*
             Example: 冒泡排序 Bubble Sort
         */
-        "bubble" : (data,mvs,infos)=>{
+        "bubble": (data, mvs, infos) => {
             let len = data.length;
             for (let i = 0; i < len - 1; i++) {
                 mvs.value.push("get(0)")
                 infos.value.push("获取arr[1]的值")
                 for (let j = 0; j < len - 1 - i; j++) {
                     // 必须保证mvs和infos的长度一致
-                    mvs.value.push("get(" + ( j + 1 ) + ")")
-                    infos.value.push("获取arr[" + ( j + 2 ) + "]的值")
-                    if (data[j] > data[j+1]) {
+                    mvs.value.push("get(" + (j + 1) + ")")
+                    infos.value.push("获取arr[" + (j + 2) + "]的值")
+                    if (data[j] > data[j + 1]) {
                         mvs.value.push("swap(" + j + "," + (j + 1) + ")")
-                        infos.value.push("arr[" + ( j + 1 ) +"]的值(" + data[j] + ")大于arr[" + ( j + 2 ) + "]的值(" + data[j+1] + ")，进行交换")
-                        let temp = data[j+1];
-                        data[j+1] = data[j];
+                        infos.value.push("arr[" + (j + 1) + "]的值(" + data[j] + ")大于arr[" + (j + 2) + "]的值(" + data[j + 1] + ")，进行交换")
+                        let temp = data[j + 1];
+                        data[j + 1] = data[j];
                         data[j] = temp;
                     }
                 }
-                if(i === len - 2){
+                if (i === len - 2) {
                     mvs.value.push("clear()")
                     infos.value.push("排序完成")
                     return
                 }
                 mvs.value.push("clear()")
-                infos.value.push("第" + ( i + 1 ) + "次循环完成")
+                infos.value.push("第" + (i + 1) + "次循环完成")
                 mvs.value.push("barrier(" + (len - i - 1) + ")")
-                infos.value.push("仅处理前" + ( len - i - 1 ) + "个数字")
+                infos.value.push("仅处理前" + (len - i - 1) + "个数字")
             }
         },
         /*
             在下方添加新增的算法
             Add new algorithms functions below
         */
+        /*
+            Example: 选择排序 Select Sort
+        */
+        "select": (data, mvs, infos) => {
+            let len = data.length;
+            for (let i = 0; i < len - 1; i++) {
+                var minIndex = i
+                for (let j = i + 1; j < len; j++) {
+                    mvs.value.push("get(" + j.toString() + ")")
+                    infos.value.push("获取arr[" + j.toString() + "]的值")
+                    if (data[minIndex] > data[j]) {
+                        minIndex = j
+                    }
+                }
+                if (minIndex === i) {
+                    mvs.value.push("barrier(" + (i + 1) + ")")
+                    infos.value.push("前" + (i + 1) + "个数字已排好序")
+                    mvs.value.push("clear()")
+                    infos.value.push("第" + (i + 1) + "次循环完成")
+                    continue
+                }
+                mvs.value.push("swap(" + i + "," + minIndex + ")")
+                infos.value.push("arr[" + minIndex + "]的值" + data[minIndex] + "最小，与第" + (i + 1) + "个进行交换")
+                let temp = data[minIndex];
+                data[minIndex] = data[i];
+                data[i] = temp;
+                mvs.value.push("barrier(" + (i + 1) + ")")
+                infos.value.push("前" + (i + 1) + "个数字已排好序")
+                mvs.value.push("clear()")
+                infos.value.push("第" + (i + 1) + "次循环完成")
+            }
+            mvs.value.push("clear()")
+            infos.value.push("排序完成")
+        }
     }
 }
