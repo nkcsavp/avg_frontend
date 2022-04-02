9dea7dac737a809c9a0fdb8c22adef673d648796
TumbledoreAlalalala <1065571228@qq.com>
2022-04-02 15:46:18 +0800

    feat:graph-dfs&bfs

--- src/components/generator/graph.js
+++ src/components/generator/graph.js
@@ -8,7 +8,9 @@ export function getGraphTypes() {
             在下方添加新增的算法名称
             Add new algorithms below
          */
-        "test": "测试占位",
+        //"test": "测试占位",
+        "dfs":"深度优先搜索",
+        "bfs":"广度优先搜索",
     }
 }
 
@@ -17,7 +19,7 @@ export function getFunctions() {
         /*
             Example: Test
         */
-        "test" : (data,rel,mvs,infos,start)=>{
+        /*"test" : (data,rel,mvs,infos,start)=>{
             mvs.value.push("emp(0)")
             infos.value.push("高亮结点 0")
             mvs.value.push("link(0,1)")
@@ -28,10 +30,82 @@ export function getFunctions() {
             infos.value.push("高亮结点 1")
             mvs.value.push("unlink(0,1)")
             infos.value.push("取消高亮 0 和 1 之间的边")
-        },
+        },*/
         /*
             在下方添加新增的算法
             Add new algorithms functions below
         */
+       "dfs":(data,rel,mvs,infos,start)=>{
+            let n =data.length;
+            
+            let dataMark=new Array(n);
+            for(let i=0;i<n;++i){
+                    dataMark[i]=1;
+            }
+            dataMark[start]=0;
+            let stack=[start];
+            mvs.value.push("emp("+start+")");
+            infos.value.push("高亮根结点");
+            while(stack.length!=0){
+                let popData=stack.pop();
+                while(dataMark[popData%n]==0&&stack.length!=0){
+                    popData=stack.pop();
+                }
+                let basicIndex=popData%n;
+                let lastIndex=parseInt(popData/n);
+
+                
+                if(basicIndex!=start){
+                    if(dataMark[popData%n]==0){
+                        break;
+                    }
+                    mvs.value.push("link("+lastIndex+","+basicIndex+")")
+                    infos.value.push("高亮 "+data[lastIndex]+" 和 "+data[basicIndex]+" 之间的边")
+                    mvs.value.push("emp("+basicIndex+")");
+                    infos.value.push("高亮"+data[basicIndex]+"节点");
+                    dataMark[basicIndex]=0;
+                }
+                for(let i=0;i<n;++i){
+                    if(dataMark[i]&&(rel[n*basicIndex+i]||rel[n*i+basicIndex])){
+                        stack.push(n*basicIndex+i);
+                    }
+                }
+            }
+       },
+
+       "bfs":(data,rel,mvs,infos,start)=>{
+            let n =data.length;
+            let dataMark=new Array(n);
+            for(let i=0;i<n;++i){
+                dataMark[i]=1;
+            }
+            dataMark[start]=0;
+            let queue=[start];
+            mvs.value.push("emp("+start+")");
+            infos.value.push("高亮根结点");
+            while(queue.length!=0){
+                let popData=queue.shift();
+                while(dataMark[popData%n]==0&&queue.length!=0){
+                    popData=queue.shift();
+                }
+                let basicIndex=popData%n;
+                let lastIndex=parseInt(popData/n);
+                if(basicIndex!=start){
+                    if(dataMark[popData%n]==0){
+                        break;
+                    }
+                    mvs.value.push("link("+lastIndex+","+basicIndex+")")
+                    infos.value.push("高亮 "+data[lastIndex]+" 和 "+data[basicIndex]+" 之间的边")
+                    mvs.value.push("emp("+basicIndex+")");
+                    infos.value.push("高亮"+data[basicIndex]+"节点");
+                    dataMark[basicIndex]=0;
+                }
+                for(let i=0;i<n;++i){
+                    if(dataMark[i]&&(rel[n*basicIndex+i]||rel[n*i+basicIndex])){
+                        queue.push(n*basicIndex+i);
+                    }
+                }
+            }
+       }
     }
 }
